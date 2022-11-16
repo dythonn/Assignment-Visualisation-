3dcd7ae1825961be33685f785aef3ec1108b4cf9
dythonn <40541477+dythonn@users.noreply.github.com>
2022-11-16 18:17:14 +0000

    Update assignment 1 (applied data science).py

--- code/assignment 1 (applied data science).py
+++ code/assignment 1 (applied data science).py
@@ -7,8 +7,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import matplotlib.animation as ani
 
 import warnings
 warnings.filterwarnings('ignore')
